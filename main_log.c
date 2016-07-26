6f2dea7baf85555e4594a13481cccfa6873fdd98
Cheng Chih Kuo <chengchihkuo@Chengde-MacBook-Air.local>
2016-07-26 23:10:50 +0800

    syslog

---
+++ main_log.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    //setlogmask();
+    openlog("testlala:", LOG_NDELAY|LOG_PID,  LOG_USER);
+    char* aa = "Hello world!\n";
+    syslog(2, "%s", aa);
+    closelog();
+    return 0;
+}
