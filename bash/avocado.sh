f6a9400f719fe893e9065185835a6a78158d32e0
bryan <cckuo.at.home@gmail.com>
2016-09-16 10:54:49 +0800

    revise

--- bash/avocado.sh
+++ bash/avocado.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-brew install homebrew/gui/meld tree ctags
+brew install homebrew/gui/meld tree ctags wget
+wget http://sourceforge.net/projects/dejavu/files/dejavu/2.37/dejavu-fonts-ttf-2.37.tar.bz2
+wget https://update.cyberduck.io/Cyberduck-5.1.0.20872.zip
+
