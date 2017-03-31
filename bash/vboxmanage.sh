1dd276d2f6df8f6e9b99b9705abf7a781cec0d31
Bryan Kuo <bryan.kuo@apexx.com.tw>
2017-03-31 14:45:42 +0800

    tracking

---
+++ bash/vboxmanage.sh
@@ -0,0 +1,7 @@
+vboxmanage controlvm "<VM Name 1>" poweroff
+vboxmanage controlvm
+vboxmanage list runningvms
+vboxmanage list vms
+vboxmanage controlvm "tt-ha2" poweroff
+vboxmanage startvm "c67i386dev" --type headless
+vboxmanage controlvm "w7u32bitMpoll" poweroff
