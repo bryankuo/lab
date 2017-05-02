4a1292ef5c311ceb9b45821006e473322e09468d
Bryan Kuo <bryan.kuo@apexx.com.tw>
2017-05-02 17:25:35 +0800

    vboxmanage cli

--- bash/vboxmanage.sh
+++ bash/vboxmanage.sh
@@ -1,9 +1,12 @@
 vboxmanage controlvm
+vboxmanage controlvm "tt-ha2v" pause
+vboxmanage controlvm "tt-ha2v" resume
+vboxmanage controlvm "w7u32bitMpoll" poweroff
 vboxmanage list runningvms
 vboxmanage list vms
 vboxmanage startvm "c67i386dev" --type headless
-vboxmanage controlvm "w7u32bitMpoll" poweroff
 vboxmanage export tt-ippbx2 -o ~/Documents/Centos68-x86/tatung/cluster/ippbx/tt-ippbx2.ova
+
 # vboxmanage list snapshot
 vboxmanage snapshot "tt-ha2v" list
 # vboxmanage restore snapshot
@@ -11,3 +14,4 @@ vboxmanage snapshot "tt-ha2v" restore "Snapshot 4"
 # create snapshot
 vboxmanage snapshot "tt-ha2v" take "Snapshot 5"
 # discard snapshot
+vboxmanage snapshot "tt-ha2v" delete "Snapshot 3"
