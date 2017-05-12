5c82fbd58e7844237f9e59a26c44ff6fb99f1563
Bryan Kuo <bryan.kuo@apexx.com.tw>
2017-05-12 17:15:25 +0800

    tracking

--- bash/mysql.sh
+++ bash/mysql.sh
@@ -4,4 +4,6 @@
 
 /usr/bin/mysqladmin -u root password @9ExxRoot
 
+mysqldump -u tatung -h localhost -p --single-transaction --no-create-info --skip-add-locks RFID type_group contact_report > RFID-tatung-massive-upload.sql
 
+mysql -u tatung -p RFID < RFID-tatung-massive-upload.sql
