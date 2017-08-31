9c8f4bcd073875008a4089577b3da88c956a0870
Bryan Kuo <bryan.kuo@apexx.com.tw>
2017-08-31 18:27:07 +0800

    revise

--- bash/setup-sshkey-exchange.sh
+++ bash/setup-sshkey-exchange.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
-DST_HOST=tt-ha2v
+# source dir.mk
+# assume user 'apexx':
+# number of user at destination is possibly more than 2 (apexx, root)...
 
+DST_HOST=tt-ha2
+
+cd ~
+# (all)
 ssh-keygen -t rsa
-(enter)
-(enter)
-# source
+# (enter)
+# (enter)
+
+# local:
+# ls -latr .ssh/id_rsa
 chmod 700 /home/$USER/.ssh/id_rsa
 
 # destination
 sudo vi /etc/ssh/sshd_config
+# assume line number the same
 sudo sed -i "42s/.*/PermitRootLogin without-password/" /etc/ssh/sshd_config
-# PermitRootLogin yes to allow login manually via ssh
-# sudo sed -i "42s/.*/PermitRootLogin yes/" /etc/ssh/sshd_config
 sudo service sshd restart
 
 # source
+# PermitRootLogin yes to allow login manually via ssh
+# sudo sed -i "42s/.*/PermitRootLogin yes/" /etc/ssh/sshd_config
+# sudo -i
 ssh-copy-id root@$DST_HOST
 ssh-copy-id $USER@$DST_HOST
 
-# destination
-# offline:
+# (offline, destination, user, directory, file):
+# ls -latr /home/
 chmod 755 /home/$USER
+# ls -latr /home/apexx/
 chmod 700 /home/$USER/.ssh/
+# ls -latr /home/apexx/.ssh/authorized_keys
 chmod 700 /home/$USER/.ssh/authorized_keys
 # online ( partition changed ):
 cp -R .ssh/ /tmp/
