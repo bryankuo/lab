2e8d0a8af3680ffd8df97f6fbbbdb0ce61669da2
Bryan Kuo <bryan.kuo@apexx.com.tw>
2017-12-20 17:56:03 +0800

    revise

--- bash/mysql.sh
+++ bash/mysql.sh
@@ -7,3 +7,8 @@
 mysqldump -u tatung -h localhost -p --single-transaction --no-create-info --skip-add-locks RFID type_group contact_report > RFID-tatung-massive-upload.sql
 
 mysql -u tatung -p RFID < RFID-tatung-massive-upload.sql
+
+# restore a single table from a full mysql mysqldump file?
+# ( https://goo.gl/kDchxA)
+# sed -n -e '/CREATE TABLE.*`device_event`/,/CREATE TABLE/p' export-tt-ha1-Administration-all.sql > device_event.sql
+# Remember to add DROP TABLE at the top and remove the DROP TABLE [next table] at the bottom of the file.
