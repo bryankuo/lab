690cf2c203ea294b4848e4864da1a84053fa552c
bryan <cckuo.at.home@gmail.com>
2018-02-24 01:10:17 +0800

    revise

--- bash/remote_mount.sh
+++ bash/remote_mount.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+if [ "$#" -ne 3 ]; then
+    echo $0" [remote] [local] [label]"
+    exit 1;
+fi
+# example:
+# ./remote_mount.sh apexx@disneyland:/home/apexx ~/Documents/disneyland4/ disneyland4
+REMOTE=$1
+LOCAL=$2
+LABEL=$3
 
 #reset mount
-umount -f /Volumes/apexx
-mkdir -pv /Volumes/apexx
+#umount -f $LOCAL
+#sudo mkdir -pv $LOCAL
 
 #mount dir
-sshfs apexx@disneyland:/home/apexx/ /Volumes/apexx -oauto_cache,reconnect,defer_permissions,noappledouble,negative_vncache,volname=apexx
+sshfs $REMOTE $LOCAL -oauto_cache,reconnect,defer_permissions,noappledouble,negative_vncache,volname=$LABEL
+
+exit 0;
