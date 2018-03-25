6f20e2ea297bd78f4672f923b448b80db89637b2
u16.04 <cckuo.at.home@gmail.com>
2018-03-25 09:17:19 +0800

    revise

--- bash/pdftk.sh
+++ bash/pdftk.sh
@@ -1,4 +1,4 @@
 # sudo apt-get install pdftk
 # pdftk commandline merge 2 pdf doc,
 pdftk etag-m.pdf etag-d.pdf output merged.pdf
-
+pdftk 2018_annual_meeting_credential_request.pdf cat 3-3 output req.pdf
