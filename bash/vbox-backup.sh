f3ea0dfad1464c416b082d16fdcee16d3ac3d8dd
Bryan Kuo <bryan.kuo@apexx.com.tw>
2018-04-27 17:45:23 +0800

    improve

--- bash/vbox-backup.sh
+++ bash/vbox-backup.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+CONFIG_DISNEYLAND_BK=y
+CONFIG_C7DEV_RL=n
+
 date
 BKDATE=`date +%Y%m%d%H%M`
-vboxmanage export "c7disneyland" -o ~/Documents/CentOS7-evaluate-pacemaker-gui-the-only-one/disneyland/c7disneyland-$BKDATE-backup.ova --ovf20 --manifest --vsys 1
-vboxmanage export "c67i386dev" -o ~/Documents/Centos68-x86/c67i386dev/c67i386dev-$BKDATE-backup.ova --ovf20 --manifest --vsys 1
-vboxmanage export "tt-ippbx1v" "tt-ippbx2v" -o ~/Documents/Centos68-x86/tatung/cluster/ippbx/tt-ippbx12v/tt-ippbx12v-$BKDATE-backup.ova --ovf20 --manifest --vsys 2
-vboxmanage export "tt-ha1v" "tt-ha2v" -o ~/Documents/Centos68-x86/tatung/cluster/ha/tt-ha12v/tt-ha12v-$BKDATE-backup.ova --ovf20 --manifest --vsys 2
+RLDATE=`date +%Y%m%d`
+
+if [ $CONFIG_DISNEYLAND_BK = "y" ]; then
+	echo "disneyland backup..."
+	vboxmanage export "disneyland" -o ~/Documents/CentOS7-evaluate-pacemaker-gui-the-only-one/disneyland/disneyland-$BKDATE-backup.ova --ovf20 --manifest --vsys 1
+fi
+
+# echo "c7dev backup ..."
+# vboxmanage export "c7dev" -o ~/Documents/CentOS7-evaluate-pacemaker-gui-the-only-one/c7dev/c7dev-$BKDATE-backup.ova --ovf20 --manifest --vsys 1
+
+if [ $CONFIG_C7DEV_RL = "y" ]; then
+	echo "c7dev release..."
+	vboxmanage export "c7dev" -o ~/Documents/CentOS7-evaluate-pacemaker-gui-the-only-one/c7dev/c7dev-$RLDATE-release.ova --ovf20 --manifest --vsys 1
+fi
+
+#echo "eins..."
+#vboxmanage export "eins" -o ~/Documents/CentOS7-evaluate-pacemaker-gui-the-only-one/eins/eins-$BKDATE-backup.ova --ovf20 --manifest --vsys 1
+# vboxmanage export "c67i386dev" -o ~/Documents/Centos68-x86/c67i386dev/c67i386dev-$BKDATE-backup.ova --ovf20 --manifest --vsys 1
+# vboxmanage export "tt-ippbx1v" "tt-ippbx2v" -o ~/Documents/Centos68-x86/tatung/cluster/ippbx/tt-ippbx12v/tt-ippbx12v-$BKDATE-backup.ova --ovf20 --manifest --vsys 2
+# vboxmanage export "tt-ha1v" "tt-ha2v" -o ~/Documents/Centos68-x86/tatung/cluster/ha/tt-ha12v/tt-ha12v-$BKDATE-backup.ova --ovf20 --manifest --vsys 2
+# bucket list:
 date
 exit 0
