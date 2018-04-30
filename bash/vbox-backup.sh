1d0d172dca7697f7dba5f1d3944e0e7dda086735
Bryan Kuo <bryan.kuo@apexx.com.tw>
2018-04-30 15:21:42 +0800

    tracking

--- bash/vbox-backup.sh
+++ bash/vbox-backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-CONFIG_DISNEYLAND_BK=y
+CONFIG_DISNEYLAND_BK=n
+CONFIG_EINS_BK=n
 CONFIG_C7DEV_RL=n
+CONFIG_C6DEV_BK=y
 
 date
 BKDATE=`date +%Y%m%d%H%M`
@@ -19,9 +21,16 @@ if [ $CONFIG_C7DEV_RL = "y" ]; then
 	vboxmanage export "c7dev" -o ~/Documents/CentOS7-evaluate-pacemaker-gui-the-only-one/c7dev/c7dev-$RLDATE-release.ova --ovf20 --manifest --vsys 1
 fi
 
-#echo "eins..."
-#vboxmanage export "eins" -o ~/Documents/CentOS7-evaluate-pacemaker-gui-the-only-one/eins/eins-$BKDATE-backup.ova --ovf20 --manifest --vsys 1
-# vboxmanage export "c67i386dev" -o ~/Documents/Centos68-x86/c67i386dev/c67i386dev-$BKDATE-backup.ova --ovf20 --manifest --vsys 1
+if [ $CONFIG_EINS_BK = "y" ]; then
+	echo "eins backup..."
+	vboxmanage export "eins" -o ~/Documents/CentOS7-evaluate-pacemaker-gui-the-only-one/eins/eins-$BKDATE-backup.ova --ovf20 --manifest --vsys 1
+fi
+
+if [ $CONFIG_C6DEV_BK = "y" ]; then
+        echo "c6dev backup..."
+	vboxmanage export "c67i386dev" -o ~/Documents/Centos68-x86/c67i386dev/c67i386dev-$BKDATE-backup.ova --ovf20 --manifest --vsys 1
+fi
+
 # vboxmanage export "tt-ippbx1v" "tt-ippbx2v" -o ~/Documents/Centos68-x86/tatung/cluster/ippbx/tt-ippbx12v/tt-ippbx12v-$BKDATE-backup.ova --ovf20 --manifest --vsys 2
 # vboxmanage export "tt-ha1v" "tt-ha2v" -o ~/Documents/Centos68-x86/tatung/cluster/ha/tt-ha12v/tt-ha12v-$BKDATE-backup.ova --ovf20 --manifest --vsys 2
 # bucket list:
