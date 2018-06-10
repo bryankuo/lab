6f415c7309e5f94b0085f7d1f6e60aa673a483a1
bryan <cckuo.at.home@gmail.com>
2018-06-10 12:20:53 +0800

    improve

--- bash/remote_mount.sh
+++ bash/remote_mount.sh
@@ -8,15 +8,23 @@ fi
 REMOTE=$1
 LOCAL=$2
 LABEL=$3
-
-#reset mount
-# umount -f ~/Documents/disneyland/
-#umount -f $LOCAL
-#sudo mkdir -pv $LOCAL
-
+num_mount=$(mount | grep ${LABEL} | wc -l)
+#echo $num_mount
+if [ $num_mount -le 0 ]; then
 #mount dir
 #sshfs apexx@disneyland:/home/apexx ~/Documents/disneyland/  -oauto_cache,reconnect,defer_permissions,noappledouble,negative_vncache,volname=disneyland
 
 sshfs $REMOTE $LOCAL -oauto_cache,reconnect,defer_permissions,noappledouble,negative_vncache,volname=$LABEL
+else
+	echo "unmount first."
+#reset mount
+# umount -f ~/Documents/disneyland/
+#umount -f $LOCAL
+#sudo mkdir -pv $LOCAL
+# rm -rf ~/Documents/disneyland/
+# umount -f ~/Documents/disneyland/  ; rm -rf ~/Documents/disneyland/
+# umount -f ~/Documents/disneyland/ && rm -rf ~/Documents/disneyland/
+# echo $(mount | grep disneyland | wc -l)
+fi
 
 exit 0;
