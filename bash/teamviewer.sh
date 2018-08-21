2ab107bdc05f0c070537b2b92128d03033f43793
Bryan Kuo <bryan@seaweed>
2018-08-21 11:26:47 +0800

    tracking

---
+++ bash/teamviewer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# restart teamviewer: ( https://goo.gl/82tSX2 )
+sudo teamviewer --daemon stop
+sudo teamviewer --daemon start
+sudo teamviewer --daemon restart
+
+# How to find my TeamViewer ID on SSH? ( https://goo.gl/SxAeo8 )
+teamviewer --passwd newPassword # testcli1234
+teamviewer -info
+exit 0
