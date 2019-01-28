c75d7a4dea60b4cf95b634a15609aed150db627a
bryan <cckuo.at.home@gmail.com>
2019-01-28 21:34:39 +0800

    creating qt materials under no support

--- bash/remote_mount.sh
+++ bash/remote_mount.sh
@@ -10,7 +10,8 @@ LOCAL=$2
 LABEL=$3
 num_mount=$(mount | grep ${LABEL} | wc -l)
 #echo $num_mount
-if [ $num_mount -le 0 ]; then
+if [ $num_mount -le "       0" ]; then
+# //TODO: trim "       0"
 #mount dir
 #sshfs apexx@disneyland:/home/apexx ~/Documents/disneyland/  -oauto_cache,reconnect,defer_permissions,noappledouble,negative_vncache,volname=disneyland
 
@@ -24,6 +25,7 @@ else
 # rm -rf ~/Documents/disneyland/
 # umount -f ~/Documents/disneyland/  ; rm -rf ~/Documents/disneyland/
 # umount -f ~/Documents/disneyland/ && rm -rf ~/Documents/disneyland/
+# umount -f ~/Documents/eins/ && rm -rf ~/Documents/eins/
 # echo $(mount | grep disneyland | wc -l)
 fi
 
