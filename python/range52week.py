12e7d290fa798700d9fbf0cea81f126d8ed2e2a7
bryan <bryankuo+bryan@users.noreply.github.com>
2021-04-09 12:50:58 +0800

    add feature

---
+++ python/range52week.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# python3 volume.py 2330
+# get listed daily average volume for one year
+# return 0: success, otherwise NG
+
+import sys, requests, time
+import urllib.request
+from datetime import timedelta,datetime
+from bs4 import BeautifulSoup
+
+# url = 'https://tw.screener.finance.yahoo.net/screener/check.html?cps0=' + sys.argv[1]
+url = "http://jsjustweb.jihsun.com.tw/z/zc/zca/zca.djhtm?a=" + sys.argv[1];
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+rows = soup.select('table .t01 tr')
+tds = rows[2].select('td')
+low52 = float(tds[5].renderContents())
+high52 = float(tds[3].renderContents())
+
+url1 = "https://tw.stock.yahoo.com/q/q?s=" + sys.argv[1];
+response = requests.get(url1)
+soup = BeautifulSoup(response.text, 'html.parser')
+table = soup.find("table", {"border": 2, "width": 750})
+rows = table.select('tr')
+row = rows[1]
+tds = row.select('b')
+quote = float(tds[0].renderContents())
+
+p_low52 = ( low52 - quote ) * 100 / quote
+p_high52 = ( high52 - quote ) * 100 / quote
+print( "current: " + str(quote) + \
+    ", 52 week range: " + \
+    str(low52) + " (" + "{:02.02f}".format(p_low52) + "%) " + \
+    " - " + \
+    str(high52)+ " (" + "{:02.02f}".format(p_high52) + "%) ")
+sys.exit(0)
