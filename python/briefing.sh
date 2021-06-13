2c94d1041992fa6128ed01b69f774fdf0a58c74b
bryan <bryankuo+bryan@users.noreply.github.com>
2021-06-13 18:05:28 +0800

    add feature

--- python/briefing.sh
+++ python/briefing.sh
@@ -47,5 +47,7 @@ then
     python3 news.py $1
     python3 gossip.py $1
     python3 float_trend.py $1
+    python3 profile.py $1
+    python3 gossip_search.py $1
 fi
 exit 0
---
+++ python/money_flow.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+watch -n 3 -c "python3 get_weighted_index.py"
+exit 0
