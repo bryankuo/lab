76c17a2ef0d510e4f250d560b2aec8269331454e
bryan <bryankuo+bryan@users.noreply.github.com>
2021-07-11 08:51:21 +0800

    scrapping by get, post, broswer, add selenium

--- python/t50_component.py
+++ python/t50_component.py
@@ -1,63 +1,46 @@
 #!/usr/bin/python3
 
-# python3 volume.py 2330
-# get listed daily average volume for one year
-# return 0: not found, assume otc
+# python3 t50_component.py
+# get TAIEX 50 components
+# return 0: success
 
 import sys, requests, time
-import urllib.request
-from datetime import timedelta,datetime
+import urllib.request, json
 from bs4 import BeautifulSoup
+# from selenium import webdriver
+# from selenium.webdriver import Safari
+from selenium import webdriver
+# 1.install selenium ( https://bit.ly/3xX2tzw )
+# 2.configure must enable the ‘Allow Remote Automation’ everytime
+#   ( https://bit.ly/3dcZawg )
+# from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support import expected_conditions as EC
 
-# last 12 months
-today = datetime.today()
-last_day = ""; first_day = "";
-n_rows = [None] * 12; months = []; avg_shares = [];
-
-# url_ftse50 = 'https://www.tpex.org.tw/web/stock/iNdex_info/gretai50/ingrid/r50cnstnt_result.php?l=zh-tw&d=110/04/16&s=0,asc,0&o=htm'
-# url = 'https://www.taifex.com.tw/cht/9/futuresQADetail'
-# url = 'https://www.yuantaetfs.com/#/FundWeights/1066'
-
-# good to scrap
-# url = "https://histock.tw/stock/taiexproportion.aspx"
-url = "https://histock.tw/stock/mscitaiwan.aspx"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.prettify())
-sys.exit(3)
-table = soup.select('table#tblStockList')
+browser = webdriver.Safari(executable_path = '/usr/bin/safaridriver')
+browser.get("https://www.yuantaetfs.com/#/FundWeights/1066")
+time.sleep(10) # wait until page fully loaded
+# stockweights
+page = browser.page_source
+soup = BeautifulSoup(page, 'html.parser')
+browser.quit()
+table = soup.find_all("table", {"id": "stockweights"})
 if ( table is None ):
     sys.exit(1)
-rows = table.findAll('tr')
+rows = table[0].select('tr')
 if ( len(rows) <= 0 ):
     sys.exit(2)
-else:
-    print( len(rows) )
-sys.exit(0)
-
-for i in range(0,12):
-    n_rows[i] = 0; shares = 0
-    url = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=html&date=' + months[i] + '&stockNo=' + sys.argv[1]
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    table_body = soup.find('tbody')
-    for tr in soup.findAll('tr')[2:]:
-        tds = tr.findAll('td')
-        shares += int(tds[1].text.replace(',', ''))
-        n_rows[i] += 1
-    if ( 0 < n_rows[i] ):
-        shares /= n_rows[i];
-        avg_shares.append(int(shares))
-        print( "daily avg# shares for " + months[i] + \
-                " is: " + "{:>10,}".format(avg_shares[i]))
-    else:
-        # possibly end of calculations
-        print("no data for " + months[i])
-        break
-    time.sleep(3)
-# // TODO: handle newly listed case, for example: 3413, prompt newly listed
-print("----- for the past year :")
-print( "daily avg# shares of " + sys.argv[1] + " is: "
-    + "{:>10,}".format(int(sum(avg_shares)/len(avg_shares))))
+for row in rows:
+    tds = row.select('td')
+    if ( 0 < len(tds) ):
+        ticker = tds[0].text.strip()
+        name = tds[1].text.strip()
+        weight = tds[2].text.strip()
+        shares = tds[3].text.strip()
+    print( \
+        ticker, ":", \
+        name, ":", \
+        weight, ":", \
+        shares )
 
 sys.exit(0)
