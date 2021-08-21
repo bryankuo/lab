b29a650281faea4ef708f7563b88fec1a3ae3c0d
bryankuo <cckuo.at.home@gmail.com>
2021-08-21 14:57:44 +0800

    sync

--- python/looping.py
+++ python/looping.py
@@ -1,16 +1,47 @@
 #!/usr/bin/env python3
 
-import sys, activity
+# python3 looping.py [list_type] [task]
+# return 0: success
 
-fname = "datafiles/listed_2.txt"
+import sys, datetime
+from activity import print_header, print_body
+
+# print('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+if len(sys.argv) < 3:
+    print("illegal # of arguments")
+    sys.exit(-1)
+
+list_type = sys.argv[1]
+task = sys.argv[2]
+
+if ( list_type != "2" ) and \
+    ( list_type != "4" ) and \
+    ( list_type != "5" ):
+    print("invalid list type")
+    sys.exit(-2)
+
+# //TODO: handle the case that it could be no data for type 5
+
+fname = "datafiles/listed_" + list_type + ".txt"
+
+def do_operation(ticker):
+    if task == "activity":
+        print_body(ticker)
+    else:
+        print(ticker+", "+task)
+
+if task == "activity":
+    print_header()
+
 line_count = 0
 i = 0
 myfile = open(fname, "r")
-myline = myfile.readline()
-while myline:
-    myline = myfile.readline()
-    activity.some_func(myline.strip())
+ticker = myfile.readline()
+while ticker:
+    do_operation(ticker.strip())
     line_count += 1
     i += 1
+    ticker = myfile.readline()
 myfile.close()
+# print('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
 sys.exit(0)
