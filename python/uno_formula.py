1bdedb7a13d4eb02e3e95df50675a7ef0c167a96
bryan <cckuo.at.home@gmail.com>
2023-11-20 01:43:13 +0800

    revise

--- python/uno_formula.py
+++ python/uno_formula.py
@@ -73,15 +73,18 @@ n_ticker = ( last_row - 2 ) + 1
 
 DIR0="./datafiles/taiex/rs"
 # theday = datetime.today().strftime('%Y%m%d')
-fname0 = "pr34above75." + yyyymmdd + '.csv'
-path0 = os.path.join(DIR0, fname0)
-outf0 = open(path0, "w")
-outf0.write("ticker:pr1w:pr1m\n")
 
+# rule 1
 fname1 = "leading75." + yyyymmdd + '.csv'
 path1 = os.path.join(DIR0, fname1)
 outf1 = open(path1, "w")
 outf1.write("ticker:pr1w:pr1m:pr3m:ytd\n")
+
+# rule 2
+fname0 = "pr34above75." + yyyymmdd + '.csv'
+path0 = os.path.join(DIR0, fname0)
+outf0 = open(path0, "w")
+outf0.write("ticker:pr1w:pr1m\n")
 
 def set_formula_1w():
     addr = "$L1"
@@ -100,19 +103,11 @@ def set_formula_1w():
         if ( 0.66 < cell.Value ):
             cell.CellBackColor = 0x3faf46
 
-        addr_1m = "M"+str(i)
-        cell_1m = active_sheet.getCellRangeByName(addr_1m)
-        if ( cell_1m.Value < 0.34 and 0.75 < cell.Value ):
-            cell.CellBackColor = 0xffff38
-            tkr = active_sheet.getCellRangeByName("A"+str(i)).String
-            outf0.write(tkr + ":" \
-                + "{:3.3f}".format(cell.Value) + ":" \
-                + "{:3.3f}".format(cell_1m.Value) + "\n")
-
         rs_1w  = active_sheet.getCellRangeByName("L"+str(i)).Value
         rs_1m  = active_sheet.getCellRangeByName("M"+str(i)).Value
         rs_3m  = active_sheet.getCellRangeByName("N"+str(i)).Value
         rs_ytd = active_sheet.getCellRangeByName("O"+str(i)).Value
+        # rule 1
         if ( 0.75 < rs_1w and 0.75 < rs_1m and 0.75 < rs_3m and 0.75 < rs_ytd ):
             tkr = active_sheet.getCellRangeByName("A"+str(i)).String
             outf1.write(tkr + ":" \
@@ -120,6 +115,16 @@ def set_formula_1w():
                 + "{:3.3f}".format(rs_1m) + ":" \
                 + "{:3.3f}".format(rs_3m) + ":" \
                 + "{:3.3f}".format(rs_ytd) + "\n")
+
+        addr_1m = "M"+str(i)
+        cell_1m = active_sheet.getCellRangeByName(addr_1m)
+        # rule 2
+        if ( cell_1m.Value < 0.34 and 0.75 < cell.Value ):
+            cell.CellBackColor = 0xffff38
+            tkr = active_sheet.getCellRangeByName("A"+str(i)).String
+            outf0.write(tkr + ":" \
+                + "{:3.3f}".format(cell.Value) + ":" \
+                + "{:3.3f}".format(cell_1m.Value) + "\n")
 
 def set_formula_1m():
     addr = "$M1"
@@ -194,7 +199,7 @@ def draw_legend():
 
     addr = "$Q2"
     cell = active_sheet.getCellRangeByName(addr)
-    cell.String = "PR75 or leading one-4th"
+    cell.String = "straight 4 PR75 or leading one-4th"
 
     addr = "$P3"
     cell = active_sheet.getCellRangeByName(addr)
