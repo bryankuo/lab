47d2fd982338a600886f6700f1d8661185361f39
bryankuo <cckuo.at.home@gmail.com>
2023-12-04 13:46:02 +0800

    revise

--- python/get_listed.sh
+++ python/get_listed.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 TIMESTAMP0=`date '+%Y/%m/%d %H:%M:%S'`
+DATE=`date '+%Y%m%d'`
+DIR0="./datafiles"
 # rm -f datafiles/listed_taiex.txt
-unlink datafiles/listed_taiex.txt
+unlink $DIR0/listed_taiex.txt
 declare -a arr=(2 4 5)
 echo "get all the tickers..."
 n_ticker=0;
@@ -12,16 +14,15 @@ do
     n_ticker=$(( $count + $n_ticker ))
 done
 echo $n_ticker "in total."
-cat datafiles/listed_2.txt >  datafiles/listed_taiex.txt
-cat datafiles/listed_4.txt >> datafiles/listed_taiex.txt
-cat datafiles/listed_5.txt >> datafiles/listed_taiex.txt
-DATE=`date '+%Y%m%d'`
-ln -sf datafiles/listed_taiex.$DATE.txt datafiles/listed_taiex.txt
-ls -lt datafiles/listed_*.txt
-wc -l $(readlink datafiles/listed_taiex.txt)
+cat $DIR0/listed_2.txt >  $DIR0/listed_taiex.$DATE.txt
+cat $DIR0/listed_4.txt >> $DIR0/listed_taiex.$DATE.txt
+cat $DIR0/listed_5.txt >> $DIR0/listed_taiex.$DATE.txt
+ln -sf $DIR0/listed_taiex.$DATE.txt $DIR0/listed_taiex.txt
+ls -lt $DIR0/listed_*.txt
+wc -l $(readlink "$DIR0/listed_taiex.txt")
 TIMESTAMP1=`date '+%Y/%m/%d %H:%M:%S'`
 echo "time: " $TIMESTAMP0 " looping start"
 echo "time: " $TIMESTAMP1 " looping end"
 echo -ne '\007'
 exit 0
-# // FIXME: use DIR0 instead
+# to shuffle call update_watchlist.sh
