170671a7c4691e01557972697cb96386619be553
bryan <cckuo.at.home@gmail.com>
2023-12-18 07:31:58 +0800

    revise

--- python/top10_indexes.py
+++ python/top10_indexes.py
@@ -13,14 +13,15 @@ gold = "https://candlecharts.com/candlestick-chart-look-up/gold-candlestick-char
 cnn_gfear = "https://edition.cnn.com/markets/fear-and-greed"
 gheatmap = "https://finviz.com/map.ashx?t=geo"
 sox = "https://www.investing.com/indices/phlx-semiconductor-candlestick#"
-twd_forex = "https://forex.tradingcharts.com/chart/US%20Dollar_New%20Taiwan%20Dollar.html?tz=CST&chartpair=US%2520Dollar_New%2520Taiwan%2520Dollar&ctype=b&movAvg1=&movAvg2=&per=1d"
+twd_forex = \
+"https://forex.tradingcharts.com/chart/US%20Dollar_New%20Taiwan%20Dollar.html?tz=CST&chartpair=US%2520Dollar_New%2520Taiwan%2520Dollar&ctype=b&movAvg1=&movAvg2=&per=w"
 
 my10idx_list = [
     us10y, c2gr, brent_crude, btcusd, gold, cnn_gfear, gheatmap, sox, twd_forex
 ]
 
 for url in my10idx_list:
-    webbrowser.open(url)    
+    webbrowser.open(url)
     input("Press Enter to continue...")
     print('\a') # beep works
 
