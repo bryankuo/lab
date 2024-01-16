7fb22c42edf5125ffb7a386c4898eced9b704f5b
bryan <cckuo.at.home@gmail.com>
2024-01-16 23:45:32 +0800

    add

---
+++ python/us_ticker_institutional_holding.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+# python3 us_ticker_institutional_holding.py [ticker]
+# scraping from file fetched and compare with twse in rs
+# \param in     YYYYMMDD.html
+# \param in     YYYYMMDD.full.csv
+# \param out    quote, volume, RS wrt. TWSE
+# return 0
+
+import sys, requests, time, os, numpy, random, csv
+from bs4 import BeautifulSoup
+from timeit import default_timer as timer
+from datetime import timedelta,datetime
+from pprint import pprint
+
+if ( len(sys.argv) < 2 ):
+    print("usage: us_ticker_institutional_holding.py [ticker]")
+    sys.exit(0)
+
+ticker = sys.argv[1]
+
+DIR0r="./datafiles/taiex/rs"
+DIR0a="./datafiles/taiex/after.market"
+
+url = "https://fintel.io/so/us/"+ticker
+print(url)
+# response = requests.get(url)
+# headers = {'User-Agent': random.choice(ua.list)}
+# response = requests.get(url, headers=headers)
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+html_path = ticker + ".html"
+# print(soup.prettify())
+outfile2 = open(html_path, "w", encoding='UTF-8')
+outfile2.write(soup.prettify())
+outfile2.close()
+print(html_path)
+
+sys.exit(0)
