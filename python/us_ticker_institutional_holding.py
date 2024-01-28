2cff0ec0d49865b0e28f33607d4eea880c1b94ff
bryankuo <cckuo.at.home@gmail.com>
2024-01-28 15:19:00 +0800

    revise

--- python/us_ticker_institutional_holding.py
+++ python/us_ticker_institutional_holding.py
@@ -19,22 +19,26 @@ if ( len(sys.argv) < 2 ):
 
 ticker = sys.argv[1]
 
-DIR0r="./datafiles/taiex/rs"
-DIR0a="./datafiles/taiex/after.market"
+DIR0="./datafiles/usa/institutional.holdings"
+
+if not os.path.exists(DIR0):
+    os.mkdir(DIR0)
+
+ofname1 = ticker + ".html"
+html_path = os.path.join(DIR0, ofname1)
 
 url = "https://fintel.io/so/us/"+ticker
-print(url)
+print("fetch {}".format(url))
 # response = requests.get(url)
 # headers = {'User-Agent': random.choice(ua.list)}
 # response = requests.get(url, headers=headers)
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-html_path = ticker + ".iholding.html"
 # print(soup.prettify())
 outfile2 = open(html_path, "w", encoding='UTF-8')
 outfile2.write(soup.prettify())
 outfile2.close()
-print(html_path)
+print("write to {}".format(html_path))
 tbs = soup.find_all("table", {"class": "table table-sm"})
 print("# tb {}".format(len(tbs)))
 
