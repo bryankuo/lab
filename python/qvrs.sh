3d36e50fe47d1b819f8f22d8ce07be67c2697542
bryankuo <cckuo.at.home@gmail.com>
2024-03-16 16:09:37 +0800

    adding feature

--- python/qvrs.sh
+++ python/qvrs.sh
@@ -13,14 +13,14 @@
 
 DATE=$(date +%Y%m%d)
 DIR0r="./datafiles/taiex/rs"
-DIR0a="./datafiles/taiex/after.market"
+DIR0="./datafiles/taiex/after.market"
 
 python3 after_market.py $DATE 0
 python3 after_market.py $DATE 1
 
 currenttime=$(date +%H:%M)
 TIME=${currenttime:0:2}${currenttime:3:2}
-cp -v "$DIR0a/$DATE.all.columns.csv" "$DIR0a/$DATE.$TIME.all.columns.csv"
+cp -v "$DIR0/$DATE.all.columns.csv" "$DIR0/$DATE.$TIME.all.columns.csv"
 
 python3 qvrs.py $DATE
 
@@ -29,21 +29,21 @@ python3 qvrs.py $DATE
 #    "$DIR0r/qvrs.$DATE.price.desc.csv"
 # done by qvrs.py
 
-sort -k1 -n -t: -o "$DIR0a/$DATE.csv" \
-    "$DIR0a/$DATE.price.desc.csv"
+sort -k1 -n -t: -o "$DIR0/$DATE.csv" \
+    "$DIR0/$DATE.price.desc.csv"
 
-cp -v "$DIR0a/$DATE.csv" "$DIR0a/$DATE.$TIME.csv"
-
-cp -v "$DIR0r/qvrs.$DATE.ticker.asc.csv" \
-    "$DIR0r/qvrs.$DATE.$TIME.ticker.asc.csv"
+cp -v "$DIR0/$DATE.csv" "$DIR0/$DATE.$TIME.csv"
 
 cp -v "$DIR0r/qvrs.$DATE.ticker.asc.csv" \
     "$DIR0r/qvrs.$DATE.$TIME.ticker.asc.csv"
 
-RIGHT_NOW=$( ls -lt $DIR0a/$DATE.????.csv | head -n 2 \
+cp -v "$DIR0r/qvrs.$DATE.ticker.asc.csv" \
+    "$DIR0r/qvrs.$DATE.$TIME.ticker.asc.csv"
+
+RIGHT_NOW=$( ls -lt $DIR0/$DATE.????.csv | head -n 2 \
     | cut -d '/' -f 5 | cut -c 1-13 | xargs | cut -d ' ' -f 1 )
 
-LAST_TRADE_DAY=$( ls -lt $DIR0a/????????.csv | head -n 2 \
+LAST_TRADE_DAY=$( ls -lt $DIR0/????????.csv | head -n 2 \
     | cut -d '/' -f 5 | cut -c 1-8 | xargs | cut -d ' ' -f 2 )
 
 python3 compare_volume.py $RIGHT_NOW $LAST_TRADE_DAY
--- python/uno/unload/slice_last_high.py
+++ python/uno/unload/slice_last_high.py
@@ -1,64 +1,125 @@
 #!/usr/bin/python
 
 # slice ticker and # days from last high
-# playground of pandas dataframe odfpy function
+#
+# \param in activity_watchlist.ods
+# \param in d1
+# \param in d0
+# \param out
 
 import sys, os, time
+
+# @see https://stackoverflow.com/a/15778297
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 import pandas as pd
 from pprint import pprint
 from datetime import datetime
 
-'''
+if ( len(sys.argv) < 3 ):
+    print("usage: slice_last_high.py [d1] [d0]")
+    sys.exit(0)
+
+d1 = sys.argv[1]
+d0 = sys.argv[2]
+
 DIR0 = "datafiles"
-activity="activity_watchlist.ods"
-ipath = os.path.join(DIR0, activity)
-print("reading {} ...".format(ipath))
-# t0 = time.time_ns() / (10 ** 9)
-# t0 = time.time_ns()
-t0 = time.time()
-# @see https://stackoverflow.com/a/18406412
-t_start = datetime.now().strftime('%Y%m%d %H:%M:%S.%f')[:-3]
-# df = pd.read_csv(ipath, sep=':', skiprows=0, header=0)
-df = pd.read_excel( \
-    ipath, sheet_name='20231211', \
-    engine="odf")
-# print("size: " + str(len(df)))
-# t1 = time.time_ns()
-# print("{:>.0f} nanoseconds".format(t1-t0))
-# print("{:,} nanoseconds".format(t1-t0))
-t1 = time.time()
-hours, rem = divmod(t1-t0, 3600)
-minutes, seconds = divmod(rem, 60)
-print("start: " + t_start)
-print( "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds) )
-# pprint(df)
-print(df.dtypes)
-df1 = df[['代號', '創新高天數']] # remark not allowed
-pprint(df1)
-'''
-ts = datetime.now().strftime('%Y%m%d')
-'''
-ndays_high = "ndays_high." + ts + ".ods"
-opath = os.path.join(".", ndays_high) # DIR0
-print("writing {} ...".format(opath))
-doc = pd.ExcelWriter(opath, engine="odf")
-df1.to_excel(doc, sheet_name="創新高天數")
-doc.close()
-'''
 DIR0a = "datafiles/taiex/after.market"
-f1 = ts + ".all.columns.csv"
+
+if False :
+    activity="activity_watchlist.ods"
+    ipath = os.path.join(DIR0, activity)
+    print("reading {} ...".format(ipath))
+    # t0 = time.time_ns() / (10 ** 9)
+    # t0 = time.time_ns()
+    t0 = time.time()
+    # @see https://stackoverflow.com/a/18406412
+    t_start = datetime.now().strftime('%Y%m%d %H:%M:%S.%f')[:-3]
+    df = pd.read_excel( \
+        ipath, sheet_name='20231211', \
+        engine="odf")
+    # print("size: " + str(len(df)))
+    # t1 = time.time_ns()
+    # print("{:>.0f} nanoseconds".format(t1-t0))
+    # print("{:,} nanoseconds".format(t1-t0))
+    t1 = time.time()
+    hours, rem = divmod(t1-t0, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("start: " + t_start)
+    print( "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds) )
+    # pprint(df)
+    # print(df.dtypes)
+    df1 = df[['代號', '創新高天數']] # remark not allowed
+    # pprint(df1)
+    # ts = datetime.now().strftime('%Y%m%d')
+
+    '''
+    ndays_high = "ndays_high." + ts + ".ods"
+    opath = os.path.join(".", ndays_high) # DIR0
+    print("writing {} ...".format(opath))
+    doc = pd.ExcelWriter(opath, engine="odf")
+    df1.to_excel(doc, sheet_name="創新高天數")
+    doc.close()
+    '''
+
+f1 = d1 + ".all.columns.csv"
 ipath1 = os.path.join(DIR0a, f1)
 if ( not os.path.exists(ipath1) ):
     print("{} not found".format(ipath1))
     sys.exit(0)
 print("reading {} ...".format(ipath1))
-# df2 = pd.read_csv(ipath1, sep=':', skiprows=0, header=['代號', '最高'])
-# df2 = pd.read_csv(ipath1, sep=':', header=['代號', '最高'])
+t0 = time.time()
+t_start = datetime.now().strftime('%Y%m%d %H:%M:%S.%f')[:-3]
 df2 = pd.read_csv(ipath1, sep=':', skiprows=0, header=0)
-df2a = df2[['代號', '最高']]
-print(df2a.dtypes)
-pprint(df2a)
+dfd1 = df2[['代號', '最高']]
+t1 = time.time()
+hours, rem = divmod(t1-t0, 3600)
+minutes, seconds = divmod(rem, 60)
+print("start: " + t_start)
+print( "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds) )
+print(dfd1.columns)
+
+f0 = d0 + ".all.columns.csv"
+ipath1 = os.path.join(DIR0a, f0)
+if ( not os.path.exists(ipath1) ):
+    print("{} not found".format(ipath1))
+    sys.exit(0)
+print("reading {} ...".format(ipath1))
+t0 = time.time()
+t_start = datetime.now().strftime('%Y%m%d %H:%M:%S.%f')[:-3]
+df0 = pd.read_csv(ipath1, sep=':', skiprows=0, header=0)
+dfd0 = df0[['代號', '最高']]
+# print(dfd0.dtypes)
+# pprint(dfd0)
+t1 = time.time()
+hours, rem = divmod(t1-t0, 3600)
+minutes, seconds = divmod(rem, 60)
+print("start: " + t_start)
+print( "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds) )
+# print(dfd0.columns)
+
+# diff = dfd1.compare(dfd0)
+# print(diff)
+
+# merged = dfd1.merge(dfd0, indicator=True, how='outer')
+# merged[merged['_merge'] == 'right_only']
+# print(merged)
+
+# newly added
+df3 = dfd1.merge(dfd0, on='代號', how='left', indicator=True)
+df3 = df3.query('_merge == "left_only"').drop('_merge', 1)
+print("newly added:")
+print(df3)
+
+# de-listed? // TODO:
+df4 = dfd0.merge(dfd1, on='代號', how='left', indicator=True)
+df4 = df4.query('_merge == "left_only"').drop('_merge', 1)
+print("de-listed:")
+print(df4)
+
 # 上一個比現在高的價格是出現在幾天之前
 # ( 負值表示創新低 )
+
 
 sys.exit(0)
