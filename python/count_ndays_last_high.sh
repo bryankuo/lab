baf2fb074028a574751cf51bd24aca943d1d8bed
bryankuo <cckuo.at.home@gmail.com>
2024-04-25 21:10:27 +0800

    add feature

--- python/count_ndays_last_high.sh
+++ python/count_ndays_last_high.sh
@@ -1,45 +1,17 @@
 #!/bin/bash
 
-# ./count_ndays_last_high.sh [ticker]
+# ./count_ndays_last_high.sh
 #
 # count # of (trade) days to get new high
 #
 # \param in ticker
 # \param in $DIR2/????????.all.columns.csv, on daily bases
 
-if [ "$#" -lt 1 ]; then
-    echo "usage: ./count_ndays_last_high.sh [ticker]"
-    exit 22 # @see https://stackoverflow.com/a/50405954
-fi
-TKR=$1
-
 DIR0="."
 DIR2="$DIR0/datafiles/taiex/after.market"
 DIR1="$DIR0/uno/unload"
 DIR3="$DIR0/datafiles"
 DIR4="/Applications/LibreOffice.app/Contents/Resources"
-
-# N_DAYS=$( ls -lt $DIR2/????????.all.columns.csv \
-#    | wc -l | xargs | cut -d " " -f1 )
-
-# grep -r --color="auto" -e "^1101" $DIR2/????????.all.columns.csv \
-#    | cut -d '/' -f 9 | sort -k 1 -r | cut -d ":" -f 1,10 \
-#    | sed 's/.all.columns.csv//g'
-
-# \
-#    | sed 's/.all.columns.csv//g'
-
-HIGHS=$( grep -r --color="auto" -e "^$TKR" $DIR2/????????.all.columns.csv \
-    | cut -d '/' -f 9 | sort -k 1 -r | cut -d ":" -f 1,10 \
-    | sed 's/.all.columns.csv//g' )
-
-# // TODO: get 1st high , 2nd high, slope
-echo $HIGHS | tr " " "\n" | while read -r line; do
-    # echo "$line"
-    TDAY=$( echo $line | cut -d ":" -f 1 )
-    DAY_HI=$( echo $line | cut -d ":" -f 2 )
-    echo $TDAY $DAY_HI
-done
 
 ./uno_launch.sh "$DIR3/activity_watchlist.ods"
 
@@ -50,13 +22,8 @@ d1=$( ls -lt $DIR2/????????.all.columns.csv | head -n 2 \
 d0=$( ls -lt $DIR2/????????.all.columns.csv | head -n 2 \
     | cut -d '/' -f 5 | cut -c 1-8 | xargs | cut -d ' ' -f 2 )
 
+# echo $d1 $d0
 python3 $DIR1/slice_last_high.py $d1 $d0
-
-$DIR4/python "$HOME/github/python/uno/load/addsheet.py" $DATE
-
-# starting from 20240116
-# ls -lt $DIR2/????????.all.columns.csv | cut -d '/' -f 5 | cut -c 1-8 | sort -r | wc -l | xargs
-# $( ls -lt $DIR2/????????.all.columns.csv | cut -d '/' -f 5 | cut -c 1-8 | sort )
-# d1=20240119; d0=20240118 // TODO: 20 -> d1
+$DIR4/python "$HOME/github/python/uno/load/addsheet.py" $d1
 
 exit 0
