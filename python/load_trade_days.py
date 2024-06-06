32c2d081cd5d735dcf0562058b1797810245013c
bryankuo <cckuo.at.home@gmail.com>
2024-06-06 18:00:26 +0800

    revise

--- python/load_trade_days.py
+++ python/load_trade_days.py
@@ -2,6 +2,8 @@
 
 #
 # python3 load_trade_days.py [d0 d1 ...]
+# list tickers where volume greater than x and touch 10d low today
+#
 
 import sys, os
 from datetime import datetime
@@ -25,20 +27,24 @@ for day in trade_days:
     df = parse_df(day)
     dfs.append(df)
 
-tkrs = dfs[0]['代號'].tolist()
+# tkrs = dfs[0]['代號'].tolist() # all
+df_mask = dfs[0][dfs[0]["成交量"] > 10000]
+tkrs = df_mask['代號'].tolist()
+
 today_10d_lows = []
 
 for tkr in tkrs:
     lows = []
     for df in dfs:
         mask = df["代號"] == tkr
-        # print("{}".format(df.loc[mask, '最低'].squeeze()))
         lows.append(df.loc[mask, '最低'].squeeze())
     val, idx = min((val, idx) for (idx, val) in enumerate(lows))
     # print("v {} i {} date {}".format(val, idx, sys.argv[1+idx]))
     if ( idx == 0 ):
         today_10d_lows.append(tkr)
 print(today_10d_lows)
-print("total {}".format(len(today_10d_lows)))
+print("touch 10d low and v > 10k today {}".format(len(today_10d_lows)))
+
+# today_10d_lows = list(set(today_10d_lows)) # distinction
 
 sys.exit(0)
